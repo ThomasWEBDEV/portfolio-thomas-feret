a0a4825e40b14fa1a9365e5987f96f071ef2bdf4
Feret Thomas <thomas.feret@hotmail.fr>
2025-11-08 21:21:08 +0100

    Fix: Version simplifiée Hero3D sans Three.js pour compatibilité Node v20

--- components/Hero3D.tsx
+++ components/Hero3D.tsx
@@ -1,31 +1,35 @@
 "use client";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Float, Text3D, Center } from "@react-three/drei";
 import { motion } from "framer-motion";
-
-function TextMesh() {
-  return (
-    <Center>
-      <Float speed={2} rotationIntensity={0.5} floatIntensity={0.5}>
-        <mesh>
-          <boxGeometry args={[8, 2, 0.5]} />
-          <meshStandardMaterial color="#3B82F6" metalness={0.8} roughness={0.2} />
-        </mesh>
-      </Float>
-    </Center>
-  );
-}
 
 export default function Hero3D() {
   return (
-    <div className="absolute inset-0 -z-10">
-      <Canvas camera={{ position: [0, 0, 10], fov: 45 }}>
-        <ambientLight intensity={0.5} />
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-        <pointLight position={[-10, -10, -10]} />
-        <TextMesh />
-        <OrbitControls enableZoom={false} enablePan={false} autoRotate autoRotateSpeed={0.5} />
-      </Canvas>
+    <div className="absolute inset-0 -z-10 overflow-hidden">
+      <motion.div
+        animate={{ 
+          rotate: 360,
+        }}
+        transition={{ 
+          duration: 20, 
+          repeat: Infinity, 
+          ease: "linear" 
+        }}
+        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      >
+        <div className="w-96 h-96 bg-blue-600/20 rounded-full blur-3xl" />
+      </motion.div>
+      <motion.div
+        animate={{ 
+          rotate: -360,
+        }}
+        transition={{ 
+          duration: 15, 
+          repeat: Infinity, 
+          ease: "linear" 
+        }}
+        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      >
+        <div className="w-72 h-72 bg-purple-600/20 rounded-full blur-3xl" />
+      </motion.div>
     </div>
   );
 }
