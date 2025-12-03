39acc71b37ab0cdb690f3bb74197cfe67a868c98
Feret Thomas <thomas.feret@hotmail.fr>
2025-12-03 19:41:42 +0100

    refactor: suppression du composant Footer

--- components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-[#fcfcfc] border-t border-[#050505]/10 py-8 text-center">
-      <p className="text-[#050505]/50 text-sm">© 2024 Thomas Feret. Tous droits réservés.</p>
-    </footer>
-  );
-}
